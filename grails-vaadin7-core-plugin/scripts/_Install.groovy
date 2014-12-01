858d33b93b1efca0f2196f93e123b46acbb72446
stephangrundner <rukyg9Seph>
2014-12-01 23:03:56 +0100

    Auto create vaadin directory on "install"

--- grails-vaadin7-core-plugin/scripts/_Install.groovy
+++ grails-vaadin7-core-plugin/scripts/_Install.groovy
@@ -1,7 +1,14 @@
 
+// Generate the VaadinConfig.groovy
 def config = new File("${basedir}/grails-app/conf/VaadinConfig.groovy")
 if (!config.exists()) {
     def pathToTemplate = "${pluginBasedir}/src/templates/VaadinConfig.groovy"
     def pathToConfigScriptClass = "${basedir}/grails-app/conf/VaadinConfig.groovy"
     ant.copy(file: pathToTemplate, tofile: pathToConfigScriptClass)
+}
+
+// Create the vaadin directory if it doesn't exist
+def vaadinDir = new File("${basedir}/grails-app/vaadin")
+if (!vaadinDir.exists()) {
+    ant.mkdir(dir: vaadinDir)
 }
