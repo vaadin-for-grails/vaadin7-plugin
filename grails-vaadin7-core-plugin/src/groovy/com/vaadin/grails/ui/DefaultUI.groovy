22b6bc44d86bcd68916f210a8b4150eafa4b1dd1
stephangrundner <rukyg8seph>
2015-01-14 21:42:26 +0100

    Refactoring

--- grails-vaadin7-core-plugin/src/groovy/com/vaadin/grails/ui/DefaultUI.groovy
+++ grails-vaadin7-core-plugin/src/groovy/com/vaadin/grails/ui/DefaultUI.groovy
@@ -1,10 +1,9 @@
 package com.vaadin.grails.ui
 
-import com.vaadin.annotations.Theme
 import com.vaadin.server.VaadinRequest
 import com.vaadin.ui.UI
 
-@Theme("valo")
+@VaadinUI
 class DefaultUI extends UI {
 
     @Override
