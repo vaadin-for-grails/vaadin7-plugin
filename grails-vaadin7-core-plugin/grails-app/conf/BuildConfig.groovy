754bcf1be3b640e6d174bee4aadc532d97b29267
stephangrundner <rukyg8seph>
2015-01-14 21:09:08 +0100

    Vaadin 7.3.9

--- grails-vaadin7-core-plugin/grails-app/conf/BuildConfig.groovy
+++ grails-vaadin7-core-plugin/grails-app/conf/BuildConfig.groovy
@@ -37,7 +37,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://maven.vaadin.com/vaadin-addons"
     }
 
-    vaadinVersion = "7.3.3"
+    vaadinVersion = "7.3.8"
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
